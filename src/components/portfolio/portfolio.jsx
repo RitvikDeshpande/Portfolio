d3788db9354714656a9805a00063a89b158d0618
RitvikDeshpande <deshpr3@mcmaster.ca>
2023-03-11 16:07:19 -0500

    Added the components for the layout of portfolio page

---
+++ src/components/portfolio/portfolio.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './portfolio.css'
+
+function portfolio() {
+    return (
+        <div>
+            Portfolio
+        </div>
+    )
+}
+
+export default portfolio
