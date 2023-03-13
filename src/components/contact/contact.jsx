b01be54a502e770785e15bd2be116fa2020cb2fd
RitvikDeshpande <deshpr3@mcmaster.ca>
2023-03-12 22:10:03 -0400

    Added EmailJS functionality so users can send emails to me

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -1,10 +1,49 @@
 import React from 'react'
 import './contact.css'
+import {HiOutlineMail} from 'react-icons/hi'
+import { useRef } from 'react';
+import emailjs from 'emailjs-com';
 
-const contact = () => {
+const Contact = () => {
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+    
+        emailjs.sendForm('service_b3f1ee9', 'template_vookkoe', form.current, '0dJYobbfCjXELVkeN')
+          .then((result) => {
+              console.log(result.text);
+          }, (error) => {
+              console.log(error.text);
+          });
+
+        e.target.reset()
+    };
+
     return (
-        <section id="contact">Contact</section>
+        <section id="contact">
+            <h5>Get in Touch</h5>
+            <h2>Contact Me</h2>
+
+            <div className="container contact__container">
+                <div className="contact__options">
+                    <article className="contact__option">
+                        <HiOutlineMail className='contact__option-icon'/>
+                        <h4>Email</h4>
+                        <h5>ritvik.desh@gmail.com</h5>
+                        <a href="mailto:ritvik.desh@gmail.com" target='_blank'>Send a message</a>
+                    </article>
+                </div>
+                {/*  END OF CONTACT OPTIONS*/}
+                <form ref={form} onSubmit={sendEmail}>
+                    <input type="text" name="name" placeholder='Your Full Name' required/>
+                    <input type="email" name="email" placeholder="Your Email" required/>
+                    <textarea name="message" rows="7" placeholder='Your Message' required></textarea>
+                    <button type='submit' className='btn btn-primary'>Send Messsage</button>
+                </form>
+            </div>
+        </section>
     )
 }
 
-export default contact
+export default Contact
--- src/components/portfolio/portfolio.jsx
+++ src/components/portfolio/portfolio.jsx
@@ -1,9 +1,82 @@
 import React from 'react'
 import './portfolio.css'
+import IMG1 from '../../assets/portfolio1.jpg'
+import IMG2 from '../../assets/portfolio2.jpg'
+import IMG3 from '../../assets/portfolio3.jpg'
+import IMG4 from '../../assets/portfolio4.jpg'
+import IMG5 from '../../assets/portfolio5.png'
+import IMG6 from '../../assets/portfolio6.jpg'
+
+const data = [
+    {
+        id: 1,
+        image: IMG1,
+        title: "Crypto Dashboard and Financial Visualizer",
+        github: 'https://github.com',
+        demo: 'https://dribbble.com/shots/20887623-Crypto-Trading-Complete-Landing-Page'
+    }, 
+    {
+        id: 2,
+        image: IMG2,
+        title: "Project 2's title will be about the following:",
+        github: 'https://github.com',
+        demo: 'https://dribbble.com/shots/20887623-Crypto-Trading-Complete-Landing-Page'
+    },
+    {
+        id: 3,
+        image: IMG3,
+        title: "Project 3's title will be about the following:",
+        github: 'https://github.com',
+        demo: 'https://dribbble.com/shots/20887623-Crypto-Trading-Complete-Landing-Page'
+    },
+    {
+        id: 4,
+        image: IMG4,
+        title: "Project 4's title will be about the following:",
+        github: 'https://github.com',
+        demo: 'https://dribbble.com/shots/20887623-Crypto-Trading-Complete-Landing-Page'
+    },
+    {
+        id: 5,
+        image: IMG5,
+        title: "Project 5's title will be about the following:",
+        github: 'https://github.com',
+        demo: 'https://dribbble.com/shots/20887623-Crypto-Trading-Complete-Landing-Page'
+    },
+    {
+        id: 6,
+        image: IMG6,
+        title: "Project 6's title will be about the following:",
+        github: 'https://github.com',
+        demo: 'https://dribbble.com/shots/20887623-Crypto-Trading-Complete-Landing-Page'
+    }
+]
 
 function portfolio() {
     return (
-        <section id="projects">Projects</section>
+        <section id="projects">
+            <h5>My Recent Work</h5>
+            <h2>Projects</h2>
+
+            <div className="container portfolio__container">
+                {
+                    data.map(({id, image, title, github, demo}) => {
+                        return (
+                            <article key={id} className="portfolio__item">
+                                <div className="portfolio__item-image">
+                                    <img src={image} alt={title}/>
+                                </div>
+                                <h3>{title}</h3>
+                                <div className="portfolio__item-cta">
+                                    <a href={github} className="btn" target='_blank'>Github</a>
+                                    <a href={demo} className="btn btn-primary" target='_blank'>Live Demo</a>
+                                </div>
+                            </article>
+                        )
+                    })
+                }
+            </div>
+        </section>
     )
 }
 
