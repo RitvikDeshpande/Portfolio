ec3aeb256a42a99c55786e62c5d6eb7a221cc3f4
RitvikDeshpande <deshpr3@mcmaster.ca>
2023-03-13 12:47:26 -0400

    added a footer to the portfolio for easy access to the different sections

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
 import './footer.css'
+import {BsLinkedin} from 'react-icons/bs'
+import {FaGithub} from 'react-icons/fa'
 
 function footer() {
     return (
-        <div>
-            Footer
-        </div>
+        <footer>
+            <a href="#" className='footer__logo'>RITVIK</a>
+            <ul className='permalinks'>
+                <li><a href="#">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#experience">Experience</a></li>
+                <li><a href="#projects">Projects</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+
+            <div className="footer__socials">
+                <a href="https://www.linkedin.com/in/ritvik-deshpande/" target="_blank"><BsLinkedin/></a>
+                <a href="https://github.com/RitvikDeshpande/" target="_blank"><FaGithub/></a>
+            </div>
+        </footer>
     )
 }
 
